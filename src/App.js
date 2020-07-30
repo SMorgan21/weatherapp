bab91e787c42dbbc242e886130c43ba70a000405
SMorgan21 <s_morgan21@hotmail.com>
2020-07-30 09:52:42 +0100

    added images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
 
+const api = {
+    key: "73bbca9d4c12b5ab2462b9569a56b2d5",
+    base: "http://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
     return (
-        <div className="App">
-
+        <div className="app">
+            <main>
+                <div className="searchBox">
+                    <input
+                        type="text"
+                        className="searchBar"
+                        placeholder="Search..."
+                    />
+                </div>
+            </main>
         </div>
     );
 }
